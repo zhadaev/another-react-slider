edd0e4358a9544e5cd0c816cf29c8e9fa7c06a66
Ivan Zhadaev <ivan.zhadaev@gmail.com>
2020-02-28 10:47:55 +0600

    Removed unused code from Dots component

--- src/components/dots/index.jsx
+++ src/components/dots/index.jsx
@@ -1,21 +1,14 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Dot from './dot'
-import HOC from '../../shared/hoc'
 
 const Dots = ({ dot, activeSlideIdx, slides }) => {
-  const CustomDot = dot && HOC(dot)
-
   return (
     <div className='another-slider__dots'>
       {
         slides.map((_, index) => (
           <div className="another-slider__dot-wrap" key={index}>
-            {
-              // dot ? 
-              // <CustomDot isActive={index === activeSlideIdx} /> :
-              <Dot dot={dot} isActive={index === activeSlideIdx} />
-            }
+            <Dot dot={dot} isActive={index === activeSlideIdx} />
           </div>
         ))
       }
